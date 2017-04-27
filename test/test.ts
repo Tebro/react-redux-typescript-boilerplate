d99ef03d1724b3939c7191bd32594eb7dfbed3d9
Richard Weber <riche.weber@gmail.com>
2017-04-27 22:34:38 +0300

    Testing basics

---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import {equal} from "assert";
+
+describe("A test", () => {
+
+    it("should always be true", () => {
+        equal(true, true, "If this fails we are truly doomed");
+    });
+
+});
